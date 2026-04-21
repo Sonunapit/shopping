65129cfa0f4152e616495216e3d5fedf76c58831
Sonu Kumar <sonu714169@gmail.com>
2026-04-21 18:08:18 +0530

    fix app.js conflict

--- Backend/Auth/src/app.js
+++ Backend/Auth/src/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
@@ -16,17 +15,4 @@ app.use(cookieParser())
 const authRoutes = require('./routes/auth.routes')
 app.use('/api/auth', authRoutes)
 
-=======
-const exprees = require('express')
-const cookieParser = require('cookie-parser')
-
-
-
-const app = exprees()
-app.use(exprees.json())
-app.use(cookieParser())
-
-const authRoutes = require('./routes/auth.routes')
-app.use('/api/auth',authRoutes)
->>>>>>> 10867fdaa6f6c3cc1ef91972707b4811cb927e7a
 module.exports = app
