da643ddc254937c623f738efe6eb043a83810acb
Sonu Kumar Napit <sonu714169@gmail.com>
2026-04-21 20:08:37 +0530

    Update app.js

--- Backend/Auth/src/app.js
+++ Backend/Auth/src/app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors({
-  origin: "https://shopping-ubwg.onrender.com",
+  origin: "https://frontend01-i8u5.onrender.com",
   credentials: true
 }))
 
